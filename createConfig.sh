5ef01c2170f47cde6d2391843db1b915fbbf083a
peterajessup <peterajessup@users.noreply.github.com>
2020-10-08 10:10:02 +1100

    Add files via upload

---
+++ createConfig.sh
@@ -0,0 +1,14 @@
+rm oc.dat
+crtype=$1
+crname=$2
+crfile=$3
+echo "apiVersion: appconnect.ibm.com/v1beta1" >> oc.dat
+echo "kind: Configuration" >> oc.dat
+echo "metadata:" >> oc.dat
+echo "  name: ${crname}" >> oc.dat
+echo "spec:" >> oc.dat
+(echo -n "  contents: "; base64 ${crfile}) >> oc.dat
+echo "  type: ${crtype}" >> oc.dat
+echo "---" >> oc.dat 
+oc apply -f oc.dat
+
