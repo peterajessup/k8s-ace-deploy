abfb5f954e0152ae34e3acb5819f03509041c5d8
peterajessup <peterajessup@users.noreply.github.com>
2020-10-08 15:25:43 +1100

    Update createConfig.sh

--- createConfig.sh
+++ createConfig.sh
@@ -6,7 +6,7 @@ echo "kind: Configuration" >> oc.dat
 echo "metadata:" >> oc.dat
 echo "  name: ${crname}" >> oc.dat
 echo "spec:" >> oc.dat
-(echo -n "  contents: "; base64 ${crfile}) | cut -c-$(tput cols)  >> oc.dat
+(echo -n "  contents: "; base64 ${crfile}) | tr -d "\n"  >> oc.dat
 echo "  type: ${crtype}" >> oc.dat
 echo "---" >> oc.dat 
 oc apply -f ./oc.dat
