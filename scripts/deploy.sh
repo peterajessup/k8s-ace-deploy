f53a372cb02b70f5e212c9ab4438d8d9d1938c4b
peterajessup <peterajessup@users.noreply.github.com>
2022-10-12 13:29:14 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,6 @@ set -e
 bash -x createConfig.sh serverconf httpsconf config/server.conf.yaml
 bash -x createConfig.sh keystore key.jks config/key.jks
 bash -x createConfig.sh setdbparms scramparms config/setdbparms.txt
-bash -x createConfig.sh policyproject kafkapolicy config/eventstreamspolicyproject.zip
 bash -x createConfig.sh policyproject mqpolicy config/MQPolicy.zip
 
 oc apply -f yaml/aceIVTDeploy.yaml
