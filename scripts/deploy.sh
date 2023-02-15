9bb214ee0b57e697ee11b4e666873dfd11309588
peterajessup <peterajessup@users.noreply.github.com>
2023-02-15 14:24:33 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,8 @@
-oc project ace
-
-oc delete Configuration key.jks
-oc delete Configuration scramparms
-oc delete Configuration httpsconf
-oc delete Configuration mqpolicy
-
-set -e
 
 bash -x createConfig.sh serverconf httpsconf config/server.conf.yaml
 bash -x createConfig.sh keystore key.jks config/key.jks
-bash -x createConfig.sh setdbparms scramparms config/setdbparms.txt
-bash -x createConfig.sh policyproject mqpolicy config/MQPolicy.zip
 
-oc apply -f yaml/aceIVTDeploy.yaml
+
+
+kubectl apply -f yaml/aceIVTDeploy.yaml -n ace
+kubectl apply -f yaml/isivt-ingress.yaml -n ace
