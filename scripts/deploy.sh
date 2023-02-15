a864c74cba63a62b1418c28c7bf8ec5119849a0f
peterajessup <peterajessup@users.noreply.github.com>
2023-02-15 15:26:53 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,3 +6,4 @@ bash -x createConfig.sh keystore key.jks config/key.jks
 
 /var/jenkins_home/kubectl apply -f yaml/aceIVTDeploy.yaml -n ace
 /var/jenkins_home/kubectl apply -f yaml/isivt-ingress.yaml -n ace
+/var/jenkins_home/kubectl apply -f yaml/isivt-admin.yaml -n ace
