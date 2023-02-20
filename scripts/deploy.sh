468208bfe7b46c2470cf9a56ee6df949c3301e16
peterajessup <peterajessup@users.noreply.github.com>
2023-02-20 12:14:20 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,3 +7,5 @@ bash -x createConfig.sh keystore key.jks config/key.jks
 /var/jenkins_home/kubectl apply -f yaml/aceIVTDeploy.yaml -n ace
 /var/jenkins_home/kubectl apply -f yaml/isivt-ingress.yaml -n ace
 /var/jenkins_home/kubectl apply -f yaml/isivt-admin.yaml -n ace
+/var/jenkins_home/kubectl set env deployment/ivtis-is MQSI_OVERRIDE_HOSTNAME=ace-ivtis.upstream-1-25-6-643d6b687ab93581d24420becac24aae-0000.au-syd.containers.appdomain.cloud MQSI_OVERRIDE_HTTPS_PORT=443 -n ace 
+
